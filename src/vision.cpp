ba3027a203a516802cb6eef2bc5fafda8b3dad64
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-02-08 15:51:33 -0500

    Stop abort

--- src/vision.cpp
+++ src/vision.cpp
@@ -13,37 +13,43 @@
 #define LEFT_RIGHT 0b00000010
 #define HEIGHT 0b00000011
 
-std::tuple<double,double,double> get_turret_pose() {
-	// get odom x,y, and heading
-	// get turret heading
-	arms::Point p =  arms::odom::getPosition();
-	//-7.5
-	//7.2
-	return std::make_tuple(p.x, p.y, arms::odom::getHeading() - turret::get_position());
-}
-std::tuple<double,double> get_goal_vector(std::tuple<double,double,double> pose) {
-	// get goal distance
-	// calculate vector to the goal
-	return std::make_tuple(0,0);
-}
-double get_latency() {
-	// get latency from the ReceiveComms class
-	return 35;
-}
 
-Oak_1_latency_compensator latency_compensator(
-	5, // max buffer of 7
-	10, // get odom position every 10ms
-	get_turret_pose, // function to get pose of the turret
-	get_goal_vector, // function to calulate the vector to the goal based on the distance
-	get_latency // function to get the latency of the current frame
-	);
+// std::tuple<double,double,double> get_turret_pose() {
+// 	// get odom x,y, and heading
+// 	// get turret heading
+// 	arms::Point p =  arms::odom::getPosition();
+// 	//-7.5
+// 	//7.2
+// 	return std::make_tuple(p.x, p.y, arms::odom::getHeading() - turret::get_position());
+// }
 
+// std::tuple<double,double> get_goal_vector(std::tuple<double,double,double> pose) {
+// 	// get goal distance
+// 	// calculate vector to the goal
+// 	return std::make_tuple(0,0);
+// }
+
+// double get_latency() {
+// 	// get latency from the ReceiveComms class
+// 	return 35;
+// }
+
+// Oak_1_latency_compensator latency_compensator(
+// 	5, // max buffer of 7
+// 	10, // get odom position every 10ms
+// 	get_turret_pose, // function to get pose of the turret
+// 	get_goal_vector, // function to calulate the vector to the goal based on the distance
+// 	get_latency // function to get the latency of the current frame
+// 	);
 
 namespace vision {
+
+    comms::ReceiveComms communication(8, 115200, START_CHAR, END_CHAR);
+
     void task() {
-        comms::ReceiveComms communication(8, 115200, START_CHAR, END_CHAR);
         communication.start();
+
+        printf("Vision Task Started\n");
 
         uint64_t color = communication.get_data(GOAL_COLOR);
 		uint64_t lr = communication.get_data(LEFT_RIGHT);
