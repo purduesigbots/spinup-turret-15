b8d2c90f01c0cca3e60e5425004779dc489b9218
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-02-09 18:44:44 -0500

    format changes

--- src/vision.cpp
+++ src/vision.cpp
@@ -12,16 +12,18 @@
 
 #define GOAL_COLOR 0b00000001
 #define LEFT_RIGHT 0b00000010
-#define HEIGHT     0b00000011
-#define WIDTH      0b00000100
+#define HEIGHT 0b00000011
+#define WIDTH 0b00000100
+
+const std::tuple<double, double> GOAL_POS = {50 * sqrt(2), 0};
 
 const double IMAGE_HEIGHT = 416;
 const double GOAL_HEIGHT = 13.87;
 const double GOAL_WIDTH = 16;
-const double FOCAL_LENGTH = 0.5; 
+const double FOCAL_LENGTH = 0.5;
 
 // Important, this is the size of the physical sensor, not its height off the
-// ground! 
+// ground!
 // https://stackoverflow.com/questions/50125574/calculate-image-size-of-an-object-from-real-size
 const double SENSOR_HEIGHT = 0.3074803;
 
@@ -35,12 +37,14 @@ void init() {
 }
 
 double get_goal_gamma() {
-  return atan2((GOAL_WIDTH/(double)communication->get_data(WIDTH) * (265 - (double)communication->get_data(LEFT_RIGHT))), get_goal_distance()) * (180 * M_1_PI);
+  return atan2((GOAL_WIDTH / (double)communication->get_data(WIDTH) *
+                (265 - (double)communication->get_data(LEFT_RIGHT))),
+               get_goal_distance()) *
+         (180 * M_1_PI);
 }
 
-double get_goal_distance()
-{
-  return (FOCAL_LENGTH * GOAL_HEIGHT * IMAGE_HEIGHT) / 
+double get_goal_distance() {
+  return (FOCAL_LENGTH * GOAL_HEIGHT * IMAGE_HEIGHT) /
          (communication->get_data(HEIGHT) * SENSOR_HEIGHT);
 }
 
@@ -49,27 +53,29 @@ std::tuple<double, double, double> get_turret_pose() {
   // get turret heading
   arms::Point p = arms::odom::getPosition();
   //-7.5
-  //7.2
-  return std::make_tuple(p.x, p.y, arms::odom::getHeading() + turret::get_angle() + get_goal_gamma());
+  // 7.2
+  return std::make_tuple(p.x, p.y,
+                         arms::odom::getHeading() + turret::get_angle() +
+                             get_goal_gamma());
 }
 
 double get_latency() {
   return 40; // 40ms latency
 }
 
-Oak_1_latency_compensator* latency_compensator;
+Oak_1_latency_compensator *latency_compensator;
 
 void task() {
   communication->start();
 
   latency_compensator = new Oak_1_latency_compensator(
-    7, // max buffer of 7
-    10, // get odom position every 10ms
-    get_turret_pose, // function to get pose of the turret
-    get_goal_distance, // function to calulate the vector to the goal based on the distance
-    get_latency, // function to get the latency of the current frame
-    false
-);
+      7,                 // max buffer of 7
+      10,                // get odom position every 10ms
+      get_turret_pose,   // function to get pose of the turret
+      get_goal_distance, // function to calulate the vector to the goal based on
+                         // the distance
+      get_latency,       // function to get the latency of the current frame
+      false);
 
   float previous_speed = 0;
 
@@ -84,7 +90,8 @@ void task() {
     uint64_t height = communication->get_data(HEIGHT);
     uint64_t width = communication->get_data(WIDTH);
 
-    double turn_degrees = latency_compensator->get_new_goal_distance(get_goal_gamma());
+    double turn_degrees =
+        latency_compensator->get_new_goal_distance(get_goal_gamma());
     if (color == 0) {
       turn_degrees = 0;
     }
@@ -100,7 +107,8 @@ void task() {
     printf("Turrent Pose %f\n", std::get<2>(get_turret_pose()));
     printf("Time:       %f\n", counter);
 
-    if (previous_height != height || previous_lr != lr || previous_color != color) {
+    if (previous_height != height || previous_lr != lr ||
+        previous_color != color) {
       counter = 0;
     } else {
       counter += 10;
@@ -111,9 +119,13 @@ void task() {
     previous_color = color;
     printf("----------------\n");
 
-    if(turret::get_angle() >= 74 ) {
+    // 0 is red
+    // 1 is blue
+    // 3 is nothing detected
+
+    if (turret::get_angle() >= 74) {
       turn_degrees = std::min(turn_degrees, 0.0);
-    } else if(turret::get_angle() <= -74) {
+    } else if (turret::get_angle() <= -74) {
       turn_degrees = std::max(turn_degrees, 0.0);
     }
 
@@ -123,15 +135,9 @@ void task() {
       turn_degrees = 0.0;
     }
 
-
-    // 0 is red
-    // 1 is blue
-    // 3 is nothing detected
-
-
     turret::move(turn_degrees * 7.5);
-    
+
     pros::delay(10);
   }
 }
-} // namespace vision      
+} // namespace vision
