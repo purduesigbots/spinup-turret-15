dd2766c73bde2430086fcd62ce7c6717674b6195
Richard Stump <rnstump@purdue.edu>
2023-03-02 21:09:48 -0500

    Update disclift

--- src/subsystems/disclift.cpp
+++ src/subsystems/disclift.cpp
@@ -9,77 +9,82 @@ using namespace pros;
 
 namespace disclift {
     
-//LOCAL DEFS:
-pros::Motor lift_motor(LIFT_MOTOR, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
-bool lifted = false; //if true, keep true until a disc is fired
-bool reachedSpeed = false;
-int targState = 0; // 0 = down, 1 = up, 2 = hold
-double liftDownPos = 7;
+    //LOCAL DEFS:
+    pros::Motor lift_motor(LIFT_MOTOR, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+    bool lifted = false; //if true, keep true until a disc is fired
+    bool reachedSpeed = false;
+    int targState = 0; // 0 = down, 1 = up, 2 = hold
 
-void discLiftUp() {
-    lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    //Prevents lifted from changing back to false momentariily, once it's set it stays until 
-    //lifted AND flyweel detects a shot
-    if(!lifted){
-        lifted = lift_motor.get_actual_velocity() < 2 && lift_motor.get_position() > 12;
-    } else if (lift_motor.get_actual_velocity() > 2){
-        lifted = false;
+    void discLiftUp(){
+        // Conditions for various states of the disc lift
+        // 
+
+
+        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+
+        //Prevents lifted from changing back to false momentariily, once it's set it stays until 
+        //lifted AND flyweel detects a shot
+        if(!lifted){
+            lifted = lift_motor.get_actual_velocity() < 2 && lift_motor.get_position() > 12;
+        } else if (lift_motor.get_actual_velocity() > 2){
+            lifted = false;
+        }
+
+        if(!reachedSpeed){
+            reachedSpeed = flywheel::at_speed();
+        } else if (!flywheel::at_speed() && lifted){
+            //Status changed + DL was up; shot detected
+            lifted = false;
+            reachedSpeed = false;
+        }
+
+        if(lifted){
+            //DISC LIFT ALL THE WAY UP FOR CURRENT NUM OF DISCS
+            lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            lift_motor.brake();
+        } else if(lift_motor.get_position() < LIFT_UP_POS){
+            lift_motor.move_voltage(12000);
+        } else{
+            lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            lift_motor.brake();
+        }
     }
-    if(!reachedSpeed){
-        reachedSpeed = flywheel::at_speed();
-    } else if (!flywheel::at_speed() && lifted){
-        //Status changed + DL was up; shot detected
+    
+    void calculatePos(){
+        // newPos = lift_motor.get_position();
+    }
+
+    void discLiftHold(){
+        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+        if(lift_motor.get_position() < LIFT_UP_POS){
+            lift_motor.move_voltage(6000);
+            // lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            // lift_motor.brake();
+        } else{
+            lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            lift_motor.brake();
+        }
+    }
+
+    void discLiftDown(){
         lifted = false;
         reachedSpeed = false;
+        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+        if(lift_motor.get_position() < LIFT_DOWN_POS + 2){
+            // Acceptable tolerance, avoid burnout
+            lift_motor.move_voltage(0);
+        } else{
+            lift_motor.move_absolute(LIFT_DOWN_POS,100);
+        }
     }
-    if(lifted){
-        //DISC LIFT ALL THE WAY UP FOR CURRENT NUM OF DISCS
-        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-        lift_motor.brake();
-    } else if((isSilva() && lift_motor.get_position() < 89) || (!isSilva() && lift_motor.get_position() < 95)){
-        lift_motor.move_voltage(12000);
-    } else{
-        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-        lift_motor.brake();
-    }
-}
 
-void calculatePos(){
-    // newPos = lift_motor.get_position();
-}
-
-void discLiftHold(){
-    lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    if((isSilva() && lift_motor.get_position() < 89) || (!isSilva() && lift_motor.get_position() < 95)){
-        lift_motor.move_voltage(6000);
-        // lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-        // lift_motor.brake();
-    } else{
-        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-        lift_motor.brake();
+    void home() {
+        lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+        lift_motor.move(-80);
+        int timestamp = pros::millis();
+        while (lift_motor.get_actual_velocity() == 0 && pros::millis() - timestamp > 2000) {
+            pros::delay(10);
+        }
+        lift_motor.tare_position();
     }
 }
-
-void discLiftDown(){
-    lifted = false;
-    reachedSpeed = false;
-    lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    if(lift_motor.get_position() < liftDownPos + 2){
-        // Acceptable tolerance, avoid burnout
-        lift_motor.move_voltage(0);
-    } else{
-        lift_motor.move_absolute(liftDownPos,100);
-    }
-}
-
-void home() {
-    lift_motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    lift_motor.move(-80);
-    int timestamp = pros::millis();
-    while (lift_motor.get_current_draw() < 1000 && pros::millis() - timestamp < 2000) {
-        pros::delay(10);
-    }
-    lift_motor.tare_position();
-}
-
-} // namespace disclift
