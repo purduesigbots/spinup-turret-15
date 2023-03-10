ba93df16676af42832a565b1e07259e6123432ae
Jack Hughes <john.barnard.hughes@gmail.com>
2023-03-10 16:22:53 -0700

    Deflector Cleanup

--- include/subsystems/deflector.hpp
+++ include/subsystems/deflector.hpp
@@ -2,26 +2,26 @@
 
 namespace deflector {
 
-/**
- * Puts the deflector in the up position
- */
-void up();
+    /**
+     * Puts the deflector in the up position
+     */
+    void up();
 
-/**
- * Puts the deflector in the down position
- */
-void down();
+    /**
+     * Puts the deflector in the down position
+     */
+    void down();
 
-/**
- * Toggle the deflector's position 
- */
-void toggle();
+    /**
+     * Toggle the deflector's position 
+     */
+    void toggle();
 
-/**
- * Returns whether the deflector is up or not
- * 
- * @return True if the deflector is the up position 
- */
-bool is_up();
+    /**
+     * Returns whether the deflector is up or not
+     * 
+     * @return True if the deflector is the up position 
+     */
+    bool is_up();
 
-} // namespace deflector
+}
