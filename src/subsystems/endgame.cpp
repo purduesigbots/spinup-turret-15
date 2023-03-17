21dd07fc01450c91e858606ca11e788e082daa9d
Jack Hughes <john.barnard.hughes@gmail.com>
2023-03-17 16:50:37 -0600

    Endgame cleanup

--- src/subsystems/endgame.cpp
+++ src/subsystems/endgame.cpp
@@ -1,20 +1,34 @@
 #include "subsystems/endgame.hpp"
-
 #include "main.h"
-
 #include "ARMS/config.h"
 
 using namespace pros;
 
 namespace endgame {
 
-ADIDigitalOut piston(ENDGAME_PISTON);
-bool is_deployed = false;
+    namespace{ //Anonymous namespace for private data and methods
+        
+        /**
+        *
+        * PRIVATE DATA
+        *
+        */ 
+        
+        //The piston used to deploy the endgame
+        ADIDigitalOut piston(ENDGAME_PISTON);
+        //State variable: turns true once the endgame is deployed
+        bool is_deployed = false;  
+    }
+    
+    /**
+    *
+    * PUBLIC METHODS (see header file for documentation)
+    *
+    */
 
-void deploy() {
-    is_deployed = true;
-    piston.set_value(true);
-    std::cout << "Endgame launched" << std::endl;
+    void deploy() {
+        is_deployed = true;
+        piston.set_value(true);
+        std::cout << "Endgame launched" << std::endl;
+    }
 }
-
-} // namespace endgame
