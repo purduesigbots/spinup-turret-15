9e0625d0aa0a485f83aceb13017cf07e217299ad
Jack Hughes <john.barnard.hughes@gmail.com>
2023-03-17 20:42:26 -0600

    Intake cleanup

--- include/subsystems/intake.hpp
+++ include/subsystems/intake.hpp
@@ -3,71 +3,56 @@
 #include "api.h"
 
 /**
- * Subsystem Dependencies: Disc Lift
+ * The intake subsystem is responsible for intaking discs.
  */
 namespace intake {
 
-/**
- * Starts the intake of the robot with a specified speed
- * 
- * @param speed RPM to run the intake at
- */
-void start(double speed);
+    /**
+    * Starts the intake of the robot with a specified speed
+    * 
+    * @param speed RPM to run the intake at
+    */
+    void start(double speed);
 
-/**
- * Stops the intake with a specified brake mode
- */
-void stop();
+    /**
+    * Stops the intake with a specified brake mode
+    */
+    void stop();
 
-/**
- * Toggles the intake of the robot
- *
- * @param speed RPM to run the intake at, if it is toggled on
- */
-void toggle(double speed);
+    /**
+    * Toggles the intake of the robot
+    *
+    * @param speed RPM to run the intake at, if it is being toggled on
+    * otherwise, the speed parameter is ignored.
+    */
+    void toggle(double speed);
 
-/**
- * Returns whether the intake is on or not
- */
-bool is_on();
+    /**
+    * Raises the intake's arm 
+    */
+    void raise_arm();
 
-/**
- * Raises the intake's arm 
- */
-void raise_arm();
+    /**
+    * Lowers the intake's arm
+    */
+    void lower_arm();
 
-/**
- * Lowers the intake's arm
- */
-void lower_arm();
+    /**
+    * Toggles the intake's arm 
+    */
+    void toggle_arm();
 
-/**
- * Toggles the intake's arm 
- */
-void toggle_arm();
+    /**
+    * Returns whether the intake is intaking or not.
+    * 
+    * @return True if the intake is intaking. 
+    */
+    bool intaking();
 
-/**
- * Returns whether the intake's arm is raised or not 
- *
- * @return True if the intake's arm is raised
- */
-bool arm_raised();
-
-bool clear();
-
-/**
- * Returns whether the intake is intaking or not.
- * 
- * @return True if the intake is intaking. 
- */
-bool intaking();
-
-/**
- * Returns whether the intake is outtaking or not, i.e., running in reverse
- * 
- * @return True if the intake is outtaking.
- */
-bool outtaking();
-
-
-} // namespace intake
+    /**
+    * Returns whether the intake is outtaking or not, i.e., running in reverse
+    * 
+    * @return True if the intake is outtaking.
+    */
+    bool outtaking();
+} 
